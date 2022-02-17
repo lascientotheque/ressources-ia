abe965b5d8c26aa5d659802e1d83bce3a9239d14
Yannael_LB <>
2022-02-17 12:46:37 +0100

    Update documentation

--- _static/gtag_1.js
+++ _static/gtag_1.js
@@ -2,4 +2,4 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-3FLECT1E86');
+  gtag('config', 'G-3FLECT1E86', {cookie_flags: 'SameSite=None;Secure'});
